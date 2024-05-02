c4985991a0f762ee326c9d306fe21a31afe27e51
neeraj negi <nnegi637673@gmail.com>
2024-05-02 16:57:48 +0530

    sticky top

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,21 +1,4 @@
-// import { useState, useEffect } from "react";
 
-// export const useFetch = (apiPath, queryTerm="") => {
-//     const [data, setData] = useState([]);
-//     const url = `https://api.themoviedb.org/3/${apiPath}?api_key=${process.env.REACT_APP_API_KEY}&query=${queryTerm}`
-
-//     useEffect(() => {
-//         async function fetchMovies(){
-//           const response = await fetch(url, { timeout: 50000 }); // Adjust timeout value as needed
-
-//           const json = await response.json();
-//           setData(json.results);
-//         }
-//         fetchMovies();
-//       }, [url])
-
-//   return { data }
-// }
 import { useState, useEffect } from "react";
 
 export const useFetch = (apiPath, queryTerm="") => {
